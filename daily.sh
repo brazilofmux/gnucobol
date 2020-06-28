f5af03c1050fb9f30df597cf753955034a025221
Stephen Dennis <brazilofmux@gmail.com>
2020-06-28 07:07:03 -0600

    Rename directories to match docker tags.

--- daily.sh
+++ daily.sh
@@ -1,8 +1,8 @@
-REP=hurriedreformist
+REP=274462252673.dkr.ecr.us-west-2.amazonaws.com
 export REP
 for i in 30 31 40
 do
     (cd $i;./daily.sh)
 done
 
-docker push $REP/gnucobol
+#docker push $REP/gnucobol
